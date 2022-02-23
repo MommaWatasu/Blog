fa964d81b40212ef483e649960d3b09755acdf7a
Documenter.jl <documenter@juliadocs.github.io>
2022-02-23 00:59:18 +0000

    build based on 2348466

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Horse-Blog","page":"Home","title":"Horse Blog","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"このブログは、MommaWatasuが備忘録的な感じで自分で書いたプログラムとかについて適当に書いてます。 使ってる言語は","category":"page"},{"location":"","page":"Home","title":"Home","text":"Julia\nRust(習得中)\nPython\nその他愉快な仲間たち（HTML等々）","category":"page"},{"location":"","page":"Home","title":"Home","text":"って感じです。 機械学習とかOS開発とかゲームプログラミングとかをやってるので、参考になれば良いです（結構OS開発なんかは日本語の情報が少ないので、奮闘してます）。 なお、JuliaのDocumenter.jl使ってる影響でドキュメント調なのは気にしない。","category":"page"}]
+}
