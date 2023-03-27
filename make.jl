5e3fa498e10aee483d9a16619d9e26250dcb1435
MommaWatasu <ascendwatson@gmail.com>
2023-03-27 23:22:37 +0900

    AGIとHorseOSについての記事を追加

--- make.jl
+++ make.jl
@@ -7,13 +7,19 @@ makedocs(;
         "MachineLearning" => [
             "HDBSCAN" => "MachineLearning/HDBSCAN.md"
         ],
+        "AGI" => [
+            "AGIについて"　=> "AGI/index.md",
+            "大脳皮質" => "AGI/CorticalColumn.md"
+        ]
         "FreeCraft" => [
             "最初に" => "FreeCraft/index.md",
             "1回目" => "FreeCraft/1st.md"
         ],
         "HorseOS" => [
             "HorseOSとは" => "HorseOS/index.md",
-            "マウスを動かす" => "HorseOS/mouse.md"
+            "マウスを動かす" => "HorseOS/mouse.md",
+            "メモリ管理" => "HorseOS/memory.md",
+            "重ね合わせ処理" => "HorseOS/layermanager1.md"
         ]
     ]
 )
